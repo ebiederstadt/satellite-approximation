f83f6afb85ddf365ec7509a1260f434209feb95f
Erik Biederstadt <biederstadterik@gmail.com>
2023-10-04 13:50:38 -0600

    Modify the equation to work with multiple images.
    The A matrix only needs to be found once, while the be vector needs to be set on a per-image basis.

--- lib/approx/include/approx/poisson.h
+++ lib/approx/include/approx/poisson.h
@@ -5,18 +5,52 @@
 using namespace givde;
 
 namespace approx {
+struct MultiChannelImage {
+    explicit MultiChannelImage(std::vector<MatX<f64>> images)
+        : images(std::move(images))
+    {
+    }
+
+    std::vector<MatX<f64>> images;
+
+    f64 operator()(size_t c, Eigen::Index row, Eigen::Index col) const
+    {
+        return images.at(c)(row, col);
+    }
+
+    MatX<f64> operator[](size_t c) const
+    {
+        return images[c];
+    }
+
+    Eigen::Index size() const
+    {
+        return images[0].size();
+    }
+
+    Eigen::Index rows() const
+    {
+        return images[0].rows();
+    }
+
+    Eigen::Index cols() const
+    {
+        return images[0].cols();
+    }
+};
+
 /**
  * Blend two images together using the poisson equation.
- * @param input_image: The original image
- * @param replacement_image: A new image that we want to blend with the original image.
+ * @param input_images: The original image(s)
+ * @param replacement_images: New image(s) that we want to blend with the original image(s).
  * @param start_row: An offset into the original image, representing the row where replacement should start
  * @param start_column: An offset into the original image, representing the column where replacement should start
  * @param sentinel_value: A value indicating that the pixel in the replacement image should not be replaced.
  *  Eigen matrices cannot have arbitrary structures, but we want to allow for arbitrary structures
  */
 void blend_images_poisson(
-    MatX<f64>& input_image,
-    MatX<f64> const &replacement_image,
+    MultiChannelImage& input_images,
+    MultiChannelImage const& replacement_images,
     int start_row, int start_column,
     f64 sentinel_value);
 }
--- lib/approx/source/poisson.cpp
+++ lib/approx/source/poisson.cpp
@@ -5,20 +5,20 @@
 namespace approx {
 auto logger = utils::create_logger("approx::poisson");
 
-void blend_images_poisson(MatX<f64>& input_image, MatX<f64> const& replacement_image, int start_row, int start_column, f64 sentinel_value)
+void blend_images_poisson(MultiChannelImage& input_images, MultiChannelImage const& replacement_image, int start_row, int start_column, f64 sentinel_value)
 {
     // Sanity checks
-    if (replacement_image.size() > input_image.size()) {
-        logger->error("Cannot solve problem: replacement image is larger than the input image ({} vs {})", replacement_image.size(), input_image.size());
+    if (replacement_image.size() > input_images.size()) {
+        logger->error("Cannot solve problem: replacement image is larger than the input image ({} vs {})", replacement_image.size(), input_images.size());
         return;
     }
-    if (start_row < 0 || start_column < 0 || start_row >= input_image.rows() || start_column >= input_image.cols()) {
+    if (start_row < 0 || start_column < 0 || start_row >= input_images.rows() || start_column >= input_images.cols()) {
         logger->error("Cannot solve problem: row/column is out of bounds. Row: {}, Column: {}", start_row, start_column);
         return;
     }
 
     auto valid_pixel = [&](Eigen::Index row, Eigen::Index col) {
-        return replacement_image(row, col) != sentinel_value;
+        return replacement_image(0, row, col) != sentinel_value;
     };
 
     // Convert from the replacement image coordinates to the main image coordinates
@@ -44,32 +44,22 @@ void blend_images_poisson(MatX<f64>& input_image, MatX<f64> const& replacement_i
     auto num_unknowns = (int)variable_numbers.size();
 
     std::vector<triplet_t> triplets;
-    VecX<f64> b(num_unknowns);
-    b.setZero();
 
     int irow = 0;
+    // The A matrix is generated from the missing area, and does not depend on the specific image we are solving for
     for (Eigen::Index row = 0; row < replacement_image.rows(); ++row) {
         for (Eigen::Index col = 0; col < replacement_image.cols(); ++col) {
             if (!valid_pixel(row, col))
                 continue;
 
             // First, take care of the left hand side of the equation
-            std::vector<index_t> neighbours = valid_neighbours(replacement_image, { row, col });
+            std::vector<index_t> neighbours = valid_neighbours(replacement_image[0], { row, col });
             triplets.emplace_back(irow, variable_numbers.at(flatten_index(row, col)), (f64)neighbours.size());
 
             for (auto const& [nrow, ncol] : neighbours) {
-                // This computes the sum of the gradient (difference) for each pixel in the replacement image
-                b(irow) += (replacement_image(row, col) - replacement_image(nrow, ncol));
-
                 // Sum over all the neighbours that are not on the boundary
                 if (valid_pixel(nrow, ncol)) {
                     triplets.emplace_back(irow, variable_numbers.at(flatten_index(nrow, ncol)), -1);
-                }
-                // If the neighbours are not part of the mask, they must be on the region boundary.
-                // In that case we will add them to the RHS of the equation, because their values are known from
-                else {
-                    index_t index_in_input = replacement_to_input(nrow, ncol);
-                    b(irow) += input_image(index_in_input.row, index_in_input.col);
                 }
             }
 
@@ -80,13 +70,47 @@ void blend_images_poisson(MatX<f64>& input_image, MatX<f64> const& replacement_i
     sparse_t A(num_unknowns, num_unknowns);
     A.setFromTriplets(triplets.begin(), triplets.end());
     cholesky_t chol(A);
-    VecX<f64> values = chol.solve(b);
 
-    // Put the new values into the image
-    for (Eigen::Index row = 0; row < replacement_image.rows(); ++row) {
-        for (Eigen::Index col = 0; col < replacement_image.cols(); ++col) {
-            auto [nrow, ncol] = replacement_to_input(row, col);
-            input_image(nrow, ncol) = replacement_image(row, col);
+    // Solve for each channel of the multi-band image
+    std::vector<VecX<f64>> solutions;
+
+    logger->debug("Solving the system for {} image channels", input_images.images.size());
+    irow = 0;
+    for (size_t c = 0; c < input_images.images.size(); ++c) {
+        VecX<f64> b(num_unknowns);
+        b.setZero();
+
+        for (Eigen::Index row = 0; row < replacement_image.rows(); ++row) {
+            for (Eigen::Index col = 0; col < replacement_image.cols(); ++col) {
+                if (!valid_pixel(row, col))
+                    continue;
+
+                std::vector<index_t> neighbours = valid_neighbours(replacement_image[0], { row, col });
+                for (auto const& [nrow, ncol] : neighbours) {
+                    // This computes the sum of the gradient (difference) for each pixel in the replacement image
+                    b(irow) += (replacement_image(c, row, col) - replacement_image(c, nrow, ncol));
+
+                    // If the neighbours are not part of the mask, they must be on the region boundary.
+                    // In that case we will add them to the RHS of the equation, because their values are known
+                    if (!valid_pixel(nrow, ncol)) {
+                        index_t index_in_input = replacement_to_input(nrow, ncol);
+                        b(irow) += input_images(c, index_in_input.row, index_in_input.col);
+                    }
+                }
+
+                irow += 1;
+            }
+        }
+
+        solutions.push_back(chol.solve(b));
+    }
+
+    // Put the new values into the images
+    for (size_t c = 0; c < input_images.images.size(); ++c) {
+        for (Eigen::Index row = 0; row < replacement_image.rows(); ++row) {
+            for (Eigen::Index col = 0; col < replacement_image.cols(); ++col) {
+                input_images.images.at(c)(row, col) = solutions.at(c)(variable_numbers.at(flatten_index(row, col)));
+            }
         }
     }
 }
