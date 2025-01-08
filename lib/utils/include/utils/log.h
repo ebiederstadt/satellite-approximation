c8b32022c7d2b3e538230d137ecf85ad3db0d232
Erik Biederstadt <biederstadterik@gmail.com>
2025-01-08 16:12:16 -0700

    Use builtin fmt support for filesystem::path

--- lib/utils/include/utils/log.h
+++ lib/utils/include/utils/log.h
@@ -11,13 +11,3 @@ namespace utils {
 std::shared_ptr<spdlog::logger> create_logger(std::string const& name);
 fs::path log_location();
 }
-
-// Using https://github.com/fmtlib/fmt/issues/2865
-template<>
-struct fmt::formatter<std::filesystem::path> : formatter<std::string_view> {
-    template<typename FormatContext>
-    auto format(std::filesystem::path const& path, FormatContext& ctx)
-    {
-        return formatter<std::string_view>::format(path.string(), ctx);
-    }
-};
